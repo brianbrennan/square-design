7a8fd474309067fc3364c6976ac76f7a4c37f3dc
Brian Brennan <brianb@cardsandpockets.com>
2015-09-02 13:19:57 -0400

    form changes

--- components/js/submit.js
+++
@@ -1,9 +0,0 @@
-function showSubmit(){
-	s('.submit-overlay').css('visibility','visible');
-	s('.submit-overlay').css('opacity','1');
-}
-
-function hideSubmit(){
-	s('.submit-overlay').css('opacity','0');
-	s('.submit-overlay').css('visibility','hidden');
-}
