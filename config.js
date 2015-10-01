5e83e7514efa943a85913bb2a09e373657b263f1
Brian Brennan <me@brianbrennan.com>
2015-10-01 09:29:51 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'secret': 'Welcometothejunglewegotfunandgames',
-	'database': 'mongodb://localhost/users',
-	'port': process.env.PORT || 8080,
-	'cloudinary_api_key': '252953896314781',
-	'cloudinary_secret': 'u2LZWfNtORMaoyrvOBQlktk7rn8'
-};
